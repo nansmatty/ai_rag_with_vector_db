398e30dcb0de78d22f093083ba871c11debc032e
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-12-29 20:43:16 +0530

    feat: add multer and pdf-parse for file handling; update .gitignore and package.json

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { kbRouter } from './routes/kbRoute';
+import { env } from 'process';
+import cors from 'cors';
+
+const app = express();
+const PORT = env.PORT || 6001;
+
+app.use(
+	cors({
+		origin: '*',
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type'],
+		credentials: false,
+	})
+);
+
+app.use(express.json({ limit: '10mb' }));
+
+app.use('/kb', kbRouter);
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
